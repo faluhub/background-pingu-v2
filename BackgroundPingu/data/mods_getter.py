670fe60be478d905b45f8af68de2349f7e0b56ee
maskers <97827489+mskrss@users.noreply.github.com>
2023-07-12 13:06:28 +0300

    modcheck api added starlight v7

--- BackgroundPingu/data/mods_getter.py
+++ BackgroundPingu/data/mods_getter.py
@@ -7,7 +7,7 @@ def get_mods(start: bool=True):
     mods = []
     res = requests.get("https://raw.githubusercontent.com/RedLime/MCSRMods/v4/meta/v4/files.json", timeout=10)
     if res.status_code == 200:
-        content = json.loads(res.text.replace('{"version":"1.1.1","game_versions":["=1.16.1"],"name":"lazystronghold-1.1.1.jar","url":"https://github.com/Gregor0410/LazyStronghold/releases/download/v1.1.1/lazystronghold-1.1.1.jar","page":"https://github.com/Gregor0410/LazyStronghold/releases/tag/v1.1.1","sha1":"8fe6b356e19baf39e334947acd31e8dd48b09c88","size":13744},','').replace('{"version":"0.0.2-RC2","game_versions":["=1.16.4"],"name":"starlight-0.0.2-RC2.jar","url":"https://github.com/PaperMC/Starlight/releases/download/0.0.2-rc2/starlight-0.0.2-RC2.jar","page":"https://github.com/PaperMC/Starlight/releases/tag/0.0.2-rc2","sha1":"8453793974acf13723cad4c37728013d734e9c34","size":83179}','{"version":"0.0.2-RC2","game_versions":["=1.16.1"],"name":"starlight-1.16.1-v7.jar","url":"https://github.com/Minecraft-Java-Edition-Speedrunning/mcsr-starlight-1.16-1.16.5/releases/download/v7/starlight-1.16.1-v7.jar","page":"https://github.com/Minecraft-Java-Edition-Speedrunning/mcsr-starlight-1.16-1.16.5/releases/tag/v7","sha1":"3d07cb436385a9d0cc89bd17dca6d19a808dc4f0","size":83179}'))
+        content = json.loads(res.text.replace('{"version":"1.1.1","game_versions":["=1.16.1"],"name":"lazystronghold-1.1.1.jar","url":"https://github.com/Gregor0410/LazyStronghold/releases/download/v1.1.1/lazystronghold-1.1.1.jar","page":"https://github.com/Gregor0410/LazyStronghold/releases/tag/v1.1.1","sha1":"8fe6b356e19baf39e334947acd31e8dd48b09c88","size":13744},',''))
         for item in content:
             if item["type"] != "fabric_mod":
                 continue
